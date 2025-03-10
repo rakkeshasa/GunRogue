f992c24a0ce93e42e3bf3ead718bf4755233a9a7
rakkeshasa <119kms@naver.com>
2025-03-10 13:44:34 +0900

    시네머신 카메라 세팅
    커서와 플레이어가 카메라에 모두 담기도록 조정

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -8,6 +8,10 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachingTargetGroup;
 
+    [Tooltip("Populate with the CursorTarget gameobject")]
+    [SerializeField]
+    private Transform cursorTarget;
+
     private void Awake()
     {
         cinemachingTargetGroup = GetComponent<CinemachineTargetGroup>();
@@ -20,11 +24,19 @@ public class CinemachineTarget : MonoBehaviour
 
     private void SetCinemachineTargetGroup()
     {
-        // 시네머신 카메라가 촬영할 TargetGroup을 생성
-        CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target { weight = 1f, radius = 1f, target = GameManager.Instance.GetPlayer().transform };
+        // 시네머신 카메라가 촬영할 TargetGroup을 생성(플레이어, 마우스)
+        // 가중치와 바운딩 박스의 반경을 세팅해 TargetGroup의 모든 요소가 화면에 들어오도록하며, 가중치를 통해 가운데 지점을 찍음
+        // 현재 플레이어와 마우스가 1:1 비율이라면 카메라의 가운데는 플레이어와 마우스의 중간 지점이 된다.
+        CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target { weight = 1f, radius = 2.5f, target = GameManager.Instance.GetPlayer().transform };
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target { weight = 1f, radius = 1f, target = cursorTarget };
 
-        CinemachineTargetGroup.Target[] cinemachingTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player };
+        CinemachineTargetGroup.Target[] cinemachingTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player, cinemachineGroupTarget_cursor };
 
         cinemachingTargetGroup.m_Targets = cinemachingTargetArray;
+    }
+
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
     }
 }
